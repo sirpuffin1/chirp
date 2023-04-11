9366337317c3edc6da8004b23702cb76b61ddc8a
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-11 15:33:56 -0700

    style(toaster): adjust position

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import { Toaster } from "react-hot-toast";
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider {...pageProps}>
-      <Toaster />
+      <Toaster position="bottom-center"/>
       <Component {...pageProps} />
     </ClerkProvider>
   );
