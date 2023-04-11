de3fac148ae51ee4e9d89206fd8ba4086264f4c5
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-11 12:35:08 -0700

    feat(index): update font post font size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ const PostView = (props: PostWithUser) => {
           <span>{`@${author.username}`}</span>
           <span>{`  · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-2xl">{post.content}</span>
       </div>
     </div>
   );
