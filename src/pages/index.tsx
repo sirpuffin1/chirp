b7cdb08313349a7804849a7f1bf865eae39aa331
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-12 14:55:00 -0700

    chore(index): prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import { type NextPage } from "next";
 import Head from "next/head";
 import { api, type RouterOutputs } from "~/utils/api";
 import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime"
+import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import LoadingPage, { Loader } from "~/components/Loader";
 import toast from "react-hot-toast";
@@ -11,16 +11,19 @@ import { z } from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-export const emojiValidator = z.string().emoji('Only emojis are allowed.').min(1).max(280);
+export const emojiValidator = z
+  .string()
+  .emoji("Only emojis are allowed.")
+  .min(1)
+  .max(280);
 
 const postInputSchema = z.object({
-  content: emojiValidator
-})
+  content: emojiValidator,
+});
 
-type PostInputSchemaType = z.infer<typeof postInputSchema>
+type PostInputSchemaType = z.infer<typeof postInputSchema>;
 
-
-dayjs.extend(relativeTime)
+dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -28,31 +31,31 @@ const CreatePostWizard = () => {
   const ctx = api.useContext();
   const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
     onSuccess: () => {
-      reset()
-      void ctx.posts.getAll.invalidate()
+      reset();
+      void ctx.posts.getAll.invalidate();
     },
     onError: (e) => {
       const errorMessage = e.data?.zodError?.fieldErrors.content;
-      if(errorMessage && errorMessage[0]) {
-        toast.error(errorMessage[0])
+      if (errorMessage && errorMessage[0]) {
+        toast.error(errorMessage[0]);
       } else {
-        toast.error("Failed to post. Please try again later!")
+        toast.error("Failed to post. Please try again later!");
       }
+    },
+  });
+
+  const { handleSubmit, register, reset, watch } = useForm<PostInputSchemaType>(
+    {
+      resolver: zodResolver(postInputSchema),
     }
-  });
+  );
 
-  
-  const { handleSubmit, register, reset, watch } = useForm<PostInputSchemaType>({
-    resolver: zodResolver(postInputSchema)
-  });
+  const content = watch("content");
 
- 
-  const content = watch("content")
+  if (!user) return null;
 
-  if(!user) return null;
-  
-  function onSubmit (data: PostInputSchemaType) {
-    mutate(data)
+  function onSubmit(data: PostInputSchemaType) {
+    mutate(data);
   }
 
   return (
@@ -64,39 +67,46 @@ const CreatePostWizard = () => {
         height={56}
         width={56}
       />
-      
-    <form onSubmit={handleSubmit(onSubmit)} className="flex w-full">
-      <input
-        placeholder="Type some emojis!"
-        className="grow bg-transparent"
-        {...register("content")}
-        disabled={isPosting}
-      />
+
+      <form onSubmit={handleSubmit(onSubmit)} className="flex w-full">
+        <input
+          placeholder="Type some emojis!"
+          className="grow bg-transparent"
+          {...register("content")}
+          disabled={isPosting}
+        />
       </form>
 
       {content && !isPosting && (
-      <button onClick={handleSubmit(onSubmit)} type="submit">Post</button>
-    )}
-      
+        <button onClick={handleSubmit(onSubmit)} type="submit">
+          Post
+        </button>
+      )}
+
       {isPosting && (
         <div className="flex items-center justify-center">
           <Loader size={20} />
         </div>
       )}
-      
     </div>
   );
-}
+};
 
-type PostWithUser = RouterOutputs["posts"]["getAll"][number]
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 
 const PostView = (props: PostWithUser) => {
-  const { post, author } = props
+  const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <Image src={author.profileImage} className="h-14 w-14 rounded-full" alt={`${author.username}'s profile image`} height={56} width={56}/>
+      <Image
+        src={author.profileImage}
+        className="h-14 w-14 rounded-full"
+        alt={`${author.username}'s profile image`}
+        height={56}
+        width={56}
+      />
       <div className="flex flex-col">
-        <div className="flex text-slate-400 gap-1">
+        <div className="flex gap-1 text-slate-400">
           <span>{`@${author.username}`}</span>
           <span>{`  · ${dayjs(post.createdAt).fromNow()}`}</span>
         </div>
@@ -104,12 +114,12 @@ const PostView = (props: PostWithUser) => {
       </div>
     </div>
   );
-}
+};
 
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (postsLoading) return <LoadingPage />
+  if (postsLoading) return <LoadingPage />;
 
   if (!data) return <div>Something went wrong</div>;
 
@@ -125,12 +135,10 @@ const Feed = () => {
 const Home: NextPage = () => {
   const { isSignedIn, isLoaded: userLoaded } = useUser();
   //Start fetching asap
-  const { data } = api.posts.getAll.useQuery()
+  const { data } = api.posts.getAll.useQuery();
 
   // Return empty div if BOTH aren't loaded since user tends to load faster
-  if (!userLoaded) return <div />
-
-  
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -139,13 +147,17 @@ const Home: NextPage = () => {
         <meta name="description" content="Generated by create-t3-app" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center h-screen">
-        <div className="w-full md:max-w-2xl h-full border-x border-slate-400">
-        <div className="border-b border-slate-400 p-4">
-          {!isSignedIn && <div className="flex justify-center"><SignInButton /></div>}
-          {!!isSignedIn && <CreatePostWizard />}
-        </div>
-        <Feed />
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl">
+          <div className="border-b border-slate-400 p-4">
+            {!isSignedIn && (
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
+            )}
+            {!!isSignedIn && <CreatePostWizard />}
+          </div>
+          <Feed />
         </div>
       </main>
     </>
