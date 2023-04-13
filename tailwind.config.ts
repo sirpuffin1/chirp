d833832c5695034e7d8aa0e4525b3ed153fb8775
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-13 11:02:32 -0700

    feat(tailwind): add hidescrollbar class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,18 @@
 import { type Config } from "tailwindcss";
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addComponents}) {
+      addComponents({
+        '.hide-scrollbar::-webkit-scrollbar': {
+          display: 'none',
+        }
+      })
+    }, { theme: { extend: {} } }) 
+  ],
 } satisfies Config;
