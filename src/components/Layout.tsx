5538d463ca25b52882f548bcc041ee0741c099d0
sirpuffin1 <angelcruz981@yahoo.com>
2023-04-13 11:21:44 -0700

    feat(layout): scrollable posts

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,8 +2,8 @@ import { type PropsWithChildren } from "react";
 
 const Layout = (props: PropsWithChildren) => {
   return (
-    <main className="flex h-screen justify-center">
-      <div className="h-full w-full border-x border-slate-400 md:max-w-2xl overflow-y-scroll">
+    <main className="overflow-hidden flex h-screen justify-center">
+      <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
         {props.children}
       </div>
     </main>
